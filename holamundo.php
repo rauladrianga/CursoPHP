cb8b06efeed0cba5dc26ac03b9570405271442f5
Raúl González <rauladrianga@gmail.com>
2014-12-31 19:44:49 -0600

    Subiendo practica PHP

---
+++ holamundo.php
@@ -0,0 +1,15 @@
+<?php
+$bienvenido= "Hola mundo desde una variable";
+$numero1=6;
+$numero2=8;
+$suma=$numero1+$numero2;
+if ($numero1 > $numero2){
+
+	echo "El numero 1 es mayor que el 2";
+
+}
+else{
+
+	echo "El numero 2 es mayor que el numero 1";
+}
+?>
---
+++ imagen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/1999/xhtml">
+<html lang="es" xlmns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>	
+	<title>Imagen</title>
+</head>
+<body>
+<?php
+$numero1=16;
+$numero2=8;
+$nombreimagen="aha.png";
+$suma=$numero1+$numero2;
+if ($numero1 > $numero2){
+
+	
+?>
+<img src="images/<?php echo $nombreimagen ?>"/>
+<?php } ?>
+</body>
+</html>
